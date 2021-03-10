80fd4fa56c53059fbc7fd116c9e94189be274b20
Ian Hads <iphands@gmail.com>
2021-03-10 13:20:35 -0500

    add ban log

--- scripts/ban.sh
+++ scripts/ban.sh
@@ -9,6 +9,7 @@ else
 fi
 
 LIST_FILE="/root/.ip.list.txt"
+BANLOG_FILE="/root/.ban.log"
 LIST_FILE_RAW="${LIST_FILE}.raw"
 LIST_FILE_TMP="${LIST_FILE}.bak"
 
@@ -39,7 +40,10 @@ echo $OLD_COUNT
 
 echo "New count:"
 wc -l $LIST_FILE | awk '{print $1}'
+echo "`wc -l $LIST_FILE | awk '{print $1}'` `date`" >> $BANLOG_FILE
 
 cp $LIST_FILE_RAW $LIST_FILE_TMP
 sort -u $LIST_FILE_TMP | fgrep -v 'from from' > $LIST_FILE_RAW
 rm $LIST_FILE_TMP
+
+
