d5f59dd1b60e4a11c11f7f8db96e63719ee02965
Ian Page Hands <iphands@gmail.com>
2022-12-31 09:51:21 -0800

    let user change speed

--- minecraft/autoclick/main.py
+++ minecraft/autoclick/main.py
@@ -1,7 +1,9 @@
 import time
 from pymouse import PyMouse
 from pynput.keyboard import Key, Listener
-from threading import Thread, Event
+from threading import Thread, Event, Lock
+
+SPEED = 1.5
 
 clicking = Event()
 clicking.clear()
@@ -10,13 +12,17 @@ running.set()
 
 
 def click():
+    global lock
+    global speed
     m = PyMouse()
     while True:
         if not running.is_set():
             print("Clicking thread exiting...")
             break
 
-        time.sleep(2.5)
+        lock.acquire()
+        time.sleep(SPEED)
+        lock.release()
 
         if not running.is_set():
             print("Clicking thread exiting...")
@@ -24,18 +30,35 @@ def click():
 
         if not clicking.is_set():
             continue
+
         print("Sending click...")
         pos = m.position()
         m.click(pos[0], pos[1])
 
 
 def on_press(key):
+    global lock
+    global SPEED
     if key == Key.f6:
         print("Start clicker")
         clicking.set()
+
     if key == Key.f7:
         print("Stop clicker")
         clicking.clear()
+
+
+    if key == Key.f9:
+        lock.acquire()
+        SPEED = SPEED * 0.90
+        print(f"- new speed {SPEED}")
+        lock.release()
+
+    if key == Key.f10:
+        lock.acquire()
+        SPEED = SPEED * 1.10
+        print(f"+ new speed {SPEED}")
+        lock.release()
 
 
 def on_release(key):
@@ -44,7 +67,7 @@ def on_release(key):
         running.clear()
         return False
 
-
+lock = Lock()
 thread = Thread(target=click)
 thread.start()
 
